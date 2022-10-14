ef33efa9f2fa53228663368dbbd53aca509ca7af
Maldini <jmmaldonado@gmail.com>
2022-10-14 22:47:57 +0200

    Added background_footage to index file
    Allows for different backgrounds for every short created.
    Also loops the source to reach the specified video length

--- mld-yt-shorts-creator.py
+++ mld-yt-shorts-creator.py
@@ -30,6 +30,7 @@ def process_index_tsv_file(options):
       video_title = row["video_title"]
       video_description = row["video_description"]
       publish_datetime = row["publish_datetime"]
+      background_footage = row["background_footage"]
       
 
       if not skip == "1":
@@ -37,9 +38,12 @@ def process_index_tsv_file(options):
         now = datetime.datetime.now()
         print(f"{now} Processing {video_title} with filename {output_filename}" )
 
+
+
         ffmpeg_command = "ffmpeg -hide_banner -nostats -loglevel error -y "
         ffmpeg_command += "-i {} ".format("audioSpectrum.mp4")
-        ffmpeg_command += "-i {} ".format(options.background_image)
+        #ffmpeg_command += "-i {} ".format(options.background_image)
+        ffmpeg_command += "-i {} ".format(background_footage)
         ffmpeg_command += "-i {} ".format("initLogo640.png")
         ffmpeg_command += "-i {} ".format("subscribe.mp4")
         ffmpeg_command += "-ss {} -i {} ".format(start_time, options.session_file)
@@ -47,8 +51,9 @@ def process_index_tsv_file(options):
         ffmpeg_command += "[0:v] colorkey=0x00C800:0.1:0.5,transpose=2, colorchannelmixer=aa=0.4 [ckey]; " #Remove green background to audioSpectrum video
         ffmpeg_command += "[3:v] colorkey=0x6de200:0.1:0.5 [ckeySubscribe]; "
         ffmpeg_command += "[ckeySubscribe] setpts=PTS-STARTPTS+5/TB [subscribe]; "
-        ffmpeg_command += "[1:v] null [fondo]; " #Changes to background image, currently a null filter to preserve it with no changes
-        ffmpeg_command += "[fondo][ckey] overlay=25:0 [fondo_y_spectrum]; "
+        ffmpeg_command += "[1:v] loop=loop=3:size=32000:start=1 [fondo]; " #Changes to background footage, currently a null filter to preserve it with no changes or scaling to 720x1280
+        ffmpeg_command += "[fondo] scale=720:1280 [fondoEscalado]; "
+        ffmpeg_command += "[fondoEscalado][ckey] overlay=25:0 [fondo_y_spectrum]; "
         ffmpeg_command += "[fondo_y_spectrum][subscribe] overlay=0:0:enable=between(t\,5\,9) [fondoSpectrumSubscribe]; "
         ffmpeg_command += "[fondoSpectrumSubscribe][2:v] overlay=40:40 [fondoEspectroYLogo]; "
         ffmpeg_command += "[fondoEspectroYLogo] drawbox=x=0:y=880:w=720:h=190:color=0x8a00c2@0.5:thickness=fill:enable=between(t\,0\,{}), ".format(text_display_time)
@@ -74,7 +79,7 @@ def process_index_tsv_file(options):
 
 if __name__ == '__main__':
   argparser.add_argument("--index_file", required=True, help="TSV file with the index of the videos to generate")
-  argparser.add_argument("--background_image", required=True, help="720x1280 image to use as still frame for the video", default="imagen720.png")
+  #argparser.add_argument("--background_image", required=True, help="720x1280 image to use as still frame for the video", default="imagen720.png")
   argparser.add_argument("--session_file", required=True, help="MP3 file with the session corresponding to the index_file")
   argparser.add_argument("--text_time", required=False, help="Time to show the tracks titles and progress bar", default=32)
   argparser.add_argument("--video_duration", required=False, help="Length of the created short video", default=32)
